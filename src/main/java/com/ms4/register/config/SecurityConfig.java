a819785e57424254eee3aa1765fabc8fcfabe77a
Habay <biodun.banjoko@yahoo.com>
2023-04-14 15:31:39 +0100

    WIP

--- src/main/java/com/ms4/register/config/SecurityConfig.java
+++ src/main/java/com/ms4/register/config/SecurityConfig.java
@@ -6,35 +6,52 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.ms4.register.services.UserService;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled=true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfig {
 
   private final UserService userService;
-
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userService)
-        .passwordEncoder(passwordEncoder());
+  
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	 http
+	 .csrf()
+	 .disable()
+	 .authorizeRequests()
+	 .antMatchers("/api/v*/authentication/**")
+	 .permitAll()
+	 .anyRequest().authenticated()
+	 .and()
+	 .formLogin();
+	return http.build();
+	 
   }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable()
-        .authorizeRequests()
-        .antMatchers("/api/v*/authentication/**")
-        .permitAll()
-        .anyRequest().authenticated()
-        .and()
-        .formLogin();
-  }
+//  @Override
+//  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//    auth.userDetailsService(userService)
+//        .passwordEncoder(passwordEncoder());
+//  }
+
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http.csrf().disable()
+//        .authorizeRequests()
+//        .antMatchers("/api/v*/authentication/**")
+//        .permitAll()
+//        .anyRequest().authenticated()
+//        .and()
+//        .formLogin();
+//  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
